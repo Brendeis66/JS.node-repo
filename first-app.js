d2bdbfef3c0aafa730c8c52987de57e1180263ba
Jay Park <fruitspunchk@gmail.com>
2023-07-18 04:16:06 +0900

    Added two files
    first-app.js, hello.txt files added

---
+++ first-app.js
@@ -0,0 +1,8 @@
+//console.log('Hello from Node.js');
+/*
+    nodejs 에서 독립적인 파일시스템 File System 사용예시
+*/
+
+// 노드중에 코어 모듈인 fs사용.
+const fs = require('fs');
+fs.writeFileSync('hello.txt'/**args1: 파일이름 */, 'Hello from Node.js'/**args2: 파일에 들어갈 내용 */);
