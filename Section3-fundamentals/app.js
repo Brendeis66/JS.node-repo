a01bb1d5a003e6ab5e73fd8376bcda531cf5fc86
Jay Park <fruitspunchk@gmail.com>
2023-08-09 15:49:46 +0900

    add

--- Section3-fundamentals/app.js
+++ Section3-fundamentals/app.js
@@ -3,6 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
+    //  서버에서 다루는 주요정보 3가지 (URL, method, headers)
     // process.exit(); //  서버 루프 프로세스 종료
 
 });
