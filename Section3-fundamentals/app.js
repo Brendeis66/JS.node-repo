a0075cb717dca97adac927bd891975b4f4bdf562
Jay Park <fruitspunchk@gmail.com>
2023-08-11 01:36:54 +0900

    add

--- Section3-fundamentals/app.js
+++ Section3-fundamentals/app.js
@@ -1,5 +1,7 @@
 // require()를 통하여 모듈 import 하기
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req, res)=>{
     //  서버에서 다루는 req 주요정보 3가지 (URL, method, headers)
@@ -8,6 +10,7 @@ const server = http.createServer((req, res)=>{
     //  process.exit(); 
 
     const url = req.url;
+    const method = req.method;
     //  url이 root일 때 만 HTML폼을 response한다.   
     if(url === '/'){
         res.write('<html>');
@@ -16,6 +19,13 @@ const server = http.createServer((req, res)=>{
         res.write('</html>');
         return res.end();
     }
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.text', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
 
     //  첫번쨰 args인 Content-Type은 브라우저가 받아드리는 기본 헤더 형태이며, 
     //  두번째 args인 text/html은 첫번째 인자의 Contents-Type 해더에 대응하는 키값을 설정하는 것이다. 
@@ -23,7 +33,7 @@ const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type', 'text/html');
 
     res.write('<html>');
-    res.write('<head><title>Response First Page</title></head>');
+    res.write('<head><title>First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
